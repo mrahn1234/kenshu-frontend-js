86b0c88762a113909926874e466f78180e12c9ca
hoang Tu <mrahn1234@gmail.com>
2021-09-30 13:11:08 +0700

    add comment for practice 10

--- practice/10/try/src/js/index.js
+++ practice/10/try/src/js/index.js
@@ -1,55 +1,87 @@
 $(document).ready(function () {
   let list = $(".list");
   let count = 1;
-  let slide_width = parseInt($(".item").css("width"));
-  let carousel_len = $(list).children().length; // length before prepend&append ,not prepend & append yet
-  let first_clone = $(list.children()[0]).clone();
-  let last_clone = $(list.children()[carousel_len - 1]).clone();
-  first_clone.attr("id", "first-clone");
-  last_clone.attr("id", "last-clone");
-  list.append(first_clone);
-  list.prepend(last_clone);
-  let re_css = (step, transition = "transform 0.3s") => {
+  let pause_carousel = false; // define flag for autoplaying
+  const next_slide_time = 2000; // define time for every slide (in autoplay)
+  const slide_width = parseInt($(".item").css("width")); // define slide width
+  const carousel_len = $(list).children().length; // length before prepend&append ,not prepend & append yet
+  let first_clone, last_clone; //define first & last node
+
+  // cloning first and last node
+  let clone_node = () => {
+    first_clone = $(list.children()[0]).clone();
+    last_clone = $(list.children()[carousel_len - 1]).clone();
+    first_clone.attr("id", "first-clone");
+    last_clone.attr("id", "last-clone");
+    list.append(first_clone);
+    list.prepend(last_clone);
+  };
+
+  // Slide animation function
+  let slide_animation = (step, transition = "transform 0.3s") => {
     list.css("transition", transition);
-    let move_distance = `translateX(-${slide_width * count}px)`;
+    let move_distance = `translateX(-${slide_width * step}px)`;
     list.css("transform", move_distance);
   };
-  re_css(count, "none");  // set first slide (count = 0: last_clone)
+
+  // next slide func
   let next_slide = () => {
     if (count >= list.children().length - 1) return;
     count++;
-    re_css(count);
+    slide_animation(count);
   };
+
+  // previous slide func
   let prev_slide = () => {
     if (count <= 0) return;
     count--;
-    re_css(count);
+    slide_animation(count);
   };
-  let pause_carousel = false;
-  let next_slide_time = 2000;
-  let autoplay = setInterval(() => {
-    if (!pause_carousel) next_slide();
-  }, next_slide_time);
-  $(".next").click(function (e) {
-    next_slide();
-  });
-  $(".prev").click(function (e) {
-    prev_slide();
-  });
-  $(".wrapper")
-    .mouseenter(() => {
-      pause_carousel = true;
-    })
-    .mouseleave(() => {
-      pause_carousel = false;
+
+  //add event, reference from example
+  let add_event = () => {
+    // event autoplay
+    let autoplay = setInterval(() => {
+      if (!pause_carousel) next_slide();
+    }, next_slide_time);
+
+    // event click next slide
+    $(".next").click((e) => {
+      next_slide();
     });
-  list[0].addEventListener("transitionend", function () {
-    if (list.children()[count].id === first_clone.attr("id")) {
-      count = 1;
-      re_css(count, "none");
-    } else if (list.children()[count].id === last_clone.attr("id")) {
-      count = carousel_len;
-      re_css(count, "none");
-    }
-  });
+
+    // event click prev slide
+    $(".prev").click((e) => {
+      prev_slide();
+    });
+
+    // stopping | continuing autoplay when hover | unhover in carousel
+    $(".wrapper")
+      .mouseenter(() => {
+        pause_carousel = true;
+      })
+      .mouseleave(() => {
+        pause_carousel = false;
+      });
+    //reset carousel
+    list[0].addEventListener("transitionend", () => {
+      if (list.children()[count].id === first_clone.attr("id")) {
+        count = 1;
+        slide_animation(count, "none");
+      } else if (list.children()[count].id === last_clone.attr("id")) {
+        count = carousel_len;
+        slide_animation(count, "none");
+      }
+    });
+  };
+  /* -----------------Execute----------------- */
+
+  // set first slide (carousel = [3,1,2,3,1] => carousel[0] = 3, carousel[1] = 1)
+  clone_node();
+  slide_animation(count, "none");
+
+  // execute add event handle
+  add_event();
+
+  /* -----------------end Execute----------------- */
 });
